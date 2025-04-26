a986416c6a7bb84b4e4ed729ee2329d12801ad04
mg97-2022 <mohamedgamalsaied1997@gmail.com>
2025-04-26 19:34:30 +0300

    fix: change signup handler

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -20,7 +20,7 @@ export class AuthController {
   @Post('signup')
   @HttpCode(HttpStatus.CREATED)
   async signup(@Body() signupDto: SignupDto): Promise<void> {
-    return this.authService.signup(signupDto);
+    await this.authService.signup(signupDto);
   }
 
   @Patch('verify-account')
